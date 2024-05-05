2a32e2d9478fa0114001ffb4ad250c264b6e6519
L. Oliphant <oliph045@umn.edu>
2024-05-05 14:48:09 -0500

    Added three conditions and values

--- PaperPlane.m
+++ PaperPlane.m
@@ -16,20 +16,13 @@ m =	0.003; % Mass (kg)
 g =	9.81; % Gravity (m/s^2)
 rho	= 1.225; % Density at sea level (kg/m^3)
 
+CL_alpha = pi * AR/(1 + sqrt(1 + (AR/2)^2)); 
 k =	1/(pi * e * AR); % Induced drag factor
 CD0 = 0.02;	% Zero-lift drag coefficient
-CLa	= pi * AR/(1 + sqrt(1 + (AR / 2)^2)); % Lift-Coefficient Slope, per rad	
 CL = sqrt(CD0/k);	% CL max 
 CD = CD0 + k * CL^2; % Drag polar
 LD_max = CL/CD; % L/D max
-Alpha =	CL/CLa; % Corresponding Angle of Attack, rad
-
-gamma_1 = -atan(1/LD_max); % Corresponding Flight Path Angle, rad
-gamma_2 = -0.5;
-gamma_3 = 0.4;
-v1 = sqrt(2 * m * g /(rho * S * (CL * cos(gamma_1) - CD * sin(gamma_1))));
-v2 = 2;
-v3 = 7.5; % Corresponding Velocity, m/s
+aoa =	CL/CL_alpha; % Angle of attack
 
 H =	2; % Initial height (m)
 R =	0;	% Initial range (m)
@@ -38,123 +31,26 @@ to = 0;	% Initial time (s)
 tf = 6;	% Final time (s)
 tspan = [to tf]; % Range of time (s)
 
-	%figure
-	%subplot(2,2,1)
-	%plot(ta,xa(:,1),tb,xb(:,1),tc,xc(:,1),td,xd(:,1))
-	%xlabel('Time, s'), ylabel('Velocity, m/s'), grid
-	%subplot(2,2,2)
-	%plot(ta,xa(:,2),tb,xb(:,2),tc,xc(:,2),td,xd(:,2))
-	%xlabel('Time, s'), ylabel('Flight Path Angle, rad'), grid
-	%subplot(2,2,3)
-	%plot(ta,xa(:,3),tb,xb(:,3),tc,xc(:,3),td,xd(:,3))
-	%xlabel('Time, s'), ylabel('Altitude, m'), grid
-	%subplot(2,2,4)
-	%plot(ta,xa(:,4),tb,xb(:,4),tc,xc(:,4),td,xd(:,4))
-	%xlabel('Time, s'), ylabel('Range, m'), grid
+% Variation in simulated parameters (three conditions)
 
+gamma_1 = -atan(1/LD_max); % Corresponding Flight Path Angle, rad
+gamma_2 = -0.5;
+gamma_3 = 0.4;
+v1 = sqrt(2 * m * g /(rho * S * (CL * cos(gamma_1) - CD * sin(gamma_1))));
+v2 = 2;
+v3 = 7.5; % Corresponding Velocity, m/s
 
+x0 = [v1; gamma_1; H; R];
+[ta, xa] = ode23('EqMotion', tspan, x0); %Calls EqMotion file
 
+x1 = [v1; gamma_2; H; R];
+[tb, xb] = ode23('EqMotion', tspan, x1);
 
+x2 = [v1; gamma_3; H; R];
+[tc, xc] = ode23('EqMotion', tspan, x2);	
 
+y1 = [v2; gamma_1; H; R];
+[ta2, xa2] = ode23('EqMotion', tspan, y1);
 
-
-% Define nominal values and expected variations
-nominal_velocity = 3.55; % m/s
-nominal_angle = -0.18; % rad
-velocity_variation = [2, 7.5]; % [lower, higher]
-angle_variation = [-0.5, 0.4]; % [lower, higher]
-
-% Case A: Plot trajectories for varying initial conditions
-figure;
-subplot(2, 1, 1); % Height vs Range subplot
-hold on;
-for condition = 1:3
-    switch condition
-        case 1 % Nominal condition
-            color = 'k';
-            velocity = nominal_velocity;
-            angle = nominal_angle;
-        case 2 % Higher condition
-            color = 'g';
-            velocity = velocity_variation(2);
-            angle = nominal_angle;
-        case 3 % Lower condition
-            color = 'r';
-            velocity = velocity_variation(1);
-            angle = nominal_angle;
-    end
-    % Simulate and plot trajectory
-    [time, range, height] = simulatePaperPlane(velocity, angle);
-    plot(range, height, color); 
-end
-xlabel('Range (m)');
-ylabel('Height (m)');
-title('Trajectories for Varying Initial Conditions');
-legend('Nominal Condition', 'Higher condition', 'Lower condition');
-
-% Perform 100 random simulations and plot trajectories
-subplot(2, 1, 2); % Height vs Range subplot
-hold on;
-for run = 1:100
-    % Generate random initial conditions
-    velocity = velocity_variation(1) + (velocity_variation(2) - velocity_variation(1)) * rand(1);
-    angle = angle_variation(1) + (angle_variation(2) - angle_variation(1)) * rand(1);
-    % Simulate and plot trajectory
-    [time, range, height] = simulatePaperPlane(velocity, angle);
-    plot(range, height, 'Color', rand(1,3)); % Random color
-end
-xlabel('Range (m)');
-ylabel('Height (m)');
-title('Random Trajectories');
-
-% Compute and plot average trajectories and derivatives
-num_trajectories = 100;
-avg_range = zeros(size(time)); % Initialize arrays for average trajectory
-avg_height = zeros(size(time));
-for run = 1:num_trajectories
-    % Simulate random trajectory
-    velocity = velocity_variation(1) + (velocity_variation(2) - velocity_variation(1)) * rand(1);
-    angle = angle_variation(1) + (angle_variation(2) - angle_variation(1)) * rand(1);
-    [time, range, height] = simulatePaperPlane(velocity, angle);
-    % Add to average trajectory
-    avg_range = avg_range + range;
-    avg_height = avg_height + height;
-end
-avg_range = avg_range / num_trajectories; % Calculate average
-avg_height = avg_height / num_trajectories;
-
-% Fit polynomial curves to average trajectory data
-degree = 3; % Choose degree for polynomial fitting
-range_poly = polyfit(time, avg_range, degree);
-height_poly = polyfit(time, avg_height, degree);
-
-% Plot average trajectories
-figure;
-subplot(2, 1, 1);
-plot(time, polyval(range_poly, time), 'b', 'LineWidth', 2);
-xlabel('Time (s)');
-ylabel('Range (m)');
-title('Average Range Trajectory');
-subplot(2, 1, 2);
-plot(time, polyval(height_poly, time), 'r', 'LineWidth', 2);
-xlabel('Time (s)');
-ylabel('Height (m)');
-title('Average Height Trajectory');
-
-% Compute and plot first-time derivatives
-range_derivative = polyder(range_poly);
-height_derivative = polyder(height_poly);
-
-figure;
-subplot(2, 1, 1);
-plot(time, polyval(range_derivative, time), 'b', 'LineWidth', 2);
-xlabel('Time (s)');
-ylabel('dR/dt (m/s)');
-title('First Time Derivative of Range');
-subplot(2, 1, 2);
-plot(time, polyval(height_derivative, time), 'r', 'LineWidth', 2);
-xlabel('Time (s)');
-ylabel('dh/dt (m/s)');
-title('First Time Derivative of Height');
-
-
+y2 = [v3; gamma_1; H; R];
+[tb2, xb2] = ode23('EqMotion', tspan, y2);
